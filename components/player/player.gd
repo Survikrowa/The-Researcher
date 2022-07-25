3c505fc7021e0c85d1d483886984ce18d3e46069
survikrowa <szymonhrabia60@gmail.com>
2022-07-26 00:06:20 +0200

    feat/add-option-to-change-player-animation-direction-based-on-cursor

--- components/player/player.gd
+++ components/player/player.gd
@@ -1,13 +1,33 @@
-extends KinematicBody2D
+extends KinematicBody2D 
 
 onready var animation = $AnimationPlayer
 onready var animation_tree = $AnimationTree
+onready var sprite = $Sprite
+onready var raycast2D = $RayCast2D
 var velocity: Vector2 = Vector2()
 var direction: Vector2 = Vector2()
-	
+var mouse_direction: Vector2 = Vector2()
+
 func _physics_process(delta):
 	handle_player_input()
+	handle_player_facing()
 
+func handle_player_facing():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_size = get_viewport().size
+
+	var mouse_pos_from_center_x = mouse_pos.x - (screen_size.x/2)
+	var mouse_pos_from_center_y = mouse_pos.y - (screen_size.y/2)
+
+	mouse_direction = Vector2(mouse_pos_from_center_x, mouse_pos_from_center_y)
+	if mouse_direction[0] > 0:
+		sprite.flip_h = false
+		raycast2D.scale.x = -.5
+	else:
+		sprite.flip_h = true
+		raycast2D.scale.x = .5
+	animation_tree.set('parameters/Idle/blend_position', mouse_direction)
+	animation_tree.set('parameters/Walk/blend_position', mouse_direction)	
 
 func handle_player_input():
 	velocity = Vector2()
@@ -28,6 +48,4 @@ func handle_player_input():
 		animation_tree.get('parameters/playback').travel('Idle')
 	else:
 		velocity = move_and_slide(normalized_velocity * 100)		
-		animation_tree.get('parameters/playback').travel('Walk')		
-		animation_tree.set('parameters/Idle/blend_position', normalized_velocity)
-		animation_tree.set('parameters/Walk/blend_position', normalized_velocity)		
+		animation_tree.get('parameters/playback').travel('Walk')
